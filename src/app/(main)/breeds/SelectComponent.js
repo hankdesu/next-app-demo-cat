c19d7816654d260f07f44ad77439d0f422be72a4
HankChou <p30205@gmail.com>
2024-03-16 20:30:59 -0700

    feat(*): Add image click function

--- src/app/(main)/breeds/SelectComponent.js
+++ src/app/(main)/breeds/SelectComponent.js
@@ -2,17 +2,21 @@
 
 import { useState } from 'react';
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 
 import Select from '../component/Select';
 import { getBreedImage } from './actions';
 
 function SelectComponent({ options }) {
+  const [breed, setBreed] = useState('');
   const [breedImage, setBreedImage] = useState('');
+  const router = useRouter();
   return (
     <>
       <Select options={options} handleChange={async (e) => {
         const value = e.currentTarget.value;
         const image = await getBreedImage(value);
+        setBreed(value)
         setBreedImage(image);
       }} />
       <div className="mt-5 aspect-h-1 aspect-w-1 w-80 overflow-hidden rounded-md lg:aspect-none group-hover:opacity-75 lg:h-80">
@@ -23,6 +27,7 @@ function SelectComponent({ options }) {
             className="h-full w-full object-cover object-center lg:h-full lg:w-full"
             width={300}
             height={300}
+            onClick={router.push(`/breeds/${breed}`)}
           />
         )}
       </div>
